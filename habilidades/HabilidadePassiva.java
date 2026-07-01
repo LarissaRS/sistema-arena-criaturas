f099be306235e8516c677db69a19b2b3ceb29ab5
LarissaRS <larissarocha20@alu.ufc.br>
2026-07-01 18:07:53 -0300

    Update HabilidadePassiva.java

--- habilidades/HabilidadePassiva.java
+++ habilidades/HabilidadePassiva.java
@@ -2,6 +2,19 @@ package habilidades;
 
 import model.Tipo;
 
+/**
+ * Interface para criaturas que possuem uma habilidade passiva.
+ *
+ * @author Larissa Rocha e João Antônio
+ */
 public interface HabilidadePassiva {
+
+    /**
+     * Calcula o multiplicador de dano conforme o tipo do inimigo.
+     *
+     * @param tipoInimigo tipo da criatura adversária.
+     * @return multiplicador de afinidade.
+     */
     double aplicarAfinidade(Tipo tipoInimigo);
+    
 }
