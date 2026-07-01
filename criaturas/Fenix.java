3a38f8a41c3edcebff882d6c0041ae9c84009b5e
LarissaRS <larissarocha20@alu.ufc.br>
2026-07-01 18:05:12 -0300

    Update Fenix.java

--- criaturas/Fenix.java
+++ criaturas/Fenix.java
@@ -6,12 +6,26 @@ import model.Criatura;
 import model.Ataque;
 import model.Tipo;
 
+/**
+ * Representa a criatura Fênix.
+ *
+ * @author Larissa Rocha e João Antônio
+ */
 public class Fenix extends Criatura implements HabilidadeEspecial, HabilidadePassiva {
 
+    /**
+     * Cria uma nova Fênix.
+     *
+     * @param nome nome da criatura.
+     */
     public Fenix(String nome){
         super(nome, Tipo.LUZ, 25, 20, 110);
     }
 
+    /**
+     * Ativa a habilidade especial da Fênix,
+     * recuperando parte de sua vida.
+     */
     @Override
     public void usarHabilidadeEspecial() {
 
@@ -20,6 +34,12 @@ public class Fenix extends Criatura implements HabilidadeEspecial, HabilidadePas
         System.out.println(getNome() + " renasceu das cinzas e recuperou 30 pontos de vida!");
     }
 
+    /**
+     * Calcula o multiplicador de dano conforme o tipo do inimigo.
+     *
+     * @param tipoInimigo tipo da criatura adversária.
+     * @return multiplicador de afinidade.
+     */
     @Override
     public double aplicarAfinidade(Tipo tipoInimigo) {
         return switch (tipoInimigo) {
