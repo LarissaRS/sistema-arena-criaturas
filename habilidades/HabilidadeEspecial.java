703267b2ca70639d0e13971e4a7fc709e1acea08
LarissaRS <larissarocha20@alu.ufc.br>
2026-07-01 18:07:30 -0300

    Update HabilidadeEspecial.java

--- habilidades/HabilidadeEspecial.java
+++ habilidades/HabilidadeEspecial.java
@@ -1,5 +1,14 @@
 package habilidades;
 
+/**
+ * Interface para criaturas que possuem uma habilidade especial.
+ *
+ * @author Larissa Rocha e João Antônio
+ */
 public interface HabilidadeEspecial {
+
+    /**
+     * Ativa a habilidade especial da criatura.
+     */
     void usarHabilidadeEspecial();
 }
