77aede9e1eba3ac5df9e0c9035a1b2c269a0d8c1
LarissaRS <larissarocha20@alu.ufc.br>
2026-07-01 18:07:02 -0300

    Update AcaoInvalidaException.java

--- exceptions/AcaoInvalidaException.java
+++ exceptions/AcaoInvalidaException.java
@@ -1,5 +1,10 @@
 package exceptions;
 
+/**
+ * Exceção para ações inválidas na batalha.
+ *
+ * @author Larissa Rocha e João Antônio
+ */
 public class AcaoInvalidaException extends Exception {
 
     public AcaoInvalidaException(String mensagem) {
